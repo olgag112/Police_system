fa1ace1fcafe9a2cc8e890b383de591aed46dcc3
olgag112 <olga@internetum.pl>
2025-01-04 14:45:46 +0100

    Create INSERT_inmates.sql

---
+++ INSERT_inmates.sql
@@ -0,0 +1,6 @@
+INSERT INTO Inmates (FullName, InmateNumber, DateOfBirth, Charges, AdmissionDate)
+VALUES
+    ('John Reed', 'I123', '1990-02-15', 'Theft', '2025-01-02'),
+    ('Alex White', 'I456', '1985-08-19', 'Assault', '2025-01-03'),
+    ('Mia Black', 'I789', '1992-06-30', 'Fraud', '2025-01-04'),
+    ('Emma Gray', 'I012', '1978-12-05', 'Arson', '2025-01-05');
