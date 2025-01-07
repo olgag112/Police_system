caa81ef437471659711c9d208df2067a46049a7e
Franek Gajda <118768757+simply-scan-the-input@users.noreply.github.com>
2025-01-07 20:11:06 +0100

    Create drop.sql

---
+++ drop.sql
@@ -0,0 +1,12 @@
+USE POLICE
+
+go
+
+drop table CleaningService;
+drop table ForensicDepartment;
+drop table InvestigationUnit;
+drop table Officers;
+drop table Supervisors;
+drop table Departments;
+drop table Incidents;
+drop table Inmates;
