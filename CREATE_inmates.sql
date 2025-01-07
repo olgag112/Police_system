d8e9de0347ab29028dc04c4776ff6bb1ac8e8b5c
Franek Gajda <118768757+simply-scan-the-input@users.noreply.github.com>
2025-01-07 20:08:13 +0100

    CREATE_inmates.sql

--- CREATE_inmates.sql
+++ CREATE_inmates.sql
@@ -1,16 +1,12 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2708
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+USE POLICE
 
-\f0\fs24 \cf0 CREATE TABLE Inmates (\
-    InmateID INT PRIMARY KEY AUTO_INCREMENT,\
-    FullName VARCHAR(255) NOT NULL,\
-    InmateNumber VARCHAR(50) NOT NULL UNIQUE,\
-    DateOfBirth DATE NOT NULL,\
-    Charges TEXT NOT NULL,\
-    AdmissionDate DATE NOT NULL\
-);\
-}
+go
+
+CREATE TABLE Inmates (
+    InmateID INT IDENTITY (1,1) PRIMARY KEY,
+    FullName VARCHAR(255) NOT NULL,
+    InmateNumber VARCHAR(50) NOT NULL UNIQUE,
+    DateOfBirth DATE NOT NULL,
+    Charges TEXT NOT NULL,
+    AdmissionDate DATE NOT NULL
+);
