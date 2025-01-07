6ef6bd6c1ed631f59c9ec049d0cfa576332bcc4d
Franek Gajda <118768757+simply-scan-the-input@users.noreply.github.com>
2025-01-07 20:12:47 +0100

    Delete CREATE_incidents.sql

--- CREATE_incidents.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Incidents (
-    IncidentID INT PRIMARY KEY AUTO_INCREMENT,
-    IncidentDateTime DATETIME NOT NULL,
-    CrimeType VARCHAR(255) NOT NULL,
-    Status VARCHAR(100) NOT NULL,
-    AssignedPolicemen TEXT,
-    Location VARCHAR(255) NOT NULL
-);
